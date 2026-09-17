c4ba394c4c17b6f70ae03c9b6c606ce64e738bc1
Jason Moore <jason.moore@fias.wa.edu.au>
2026-09-17 12:00:49 +0800

    Refactor code structure and update Wonde client usage
    - Grouped using directives under a region; added missing namespaces
    - Changed class visibility from public to internal
    - Updated Wonde.Client instantiation to use simplified Client and ConfigurationManager
    - Improved code formatting and added braces for if statements
    - Changed iterate method parameter type to ResultIterator
    - Enhanced type casting and output formatting in iterate method
    - Removed unnecessary usings and cleaned up code structure

--- src/DotNetClient/TestWondeClient/Program.cs
+++ src/DotNetClient/TestWondeClient/Program.cs
@@ -1,32 +1,41 @@
-﻿using System;
-using System.Timers;
-using System.Collections.Generic;
+﻿// TestWondeConsole
 
 namespace TestWondeClient
 {
-    class Program
+    #region Using Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using Wonde;
+    #endregion
+
+    internal class Program
     {
         static void Main(string[] args)
         {
-
-            
-            var client = new Wonde.Client(System.Configuration.ConfigurationManager.AppSettings["API_TOKEN"]);
-            var school = client.school(System.Configuration.ConfigurationManager.AppSettings["SCHOOL_ID"]);
+            var client = new Client(ConfigurationManager.AppSettings["API_TOKEN"]);
+            var school = client.school(ConfigurationManager.AppSettings["SCHOOL_ID"]);
 
             var students = school.students.all();
 
             iterate(students);
             if (students.nextPage())
+            {
                 iterate(students);
+            }
+
             if (students.previousPage())
+            {
                 iterate(students);
+            }
         }
 
-        static void iterate(Wonde.ResultIterator students)
+        static void iterate(ResultIterator students)
         {
-            foreach (object row in students)
+            foreach (var row in students)
             {
-                var obj = (Dictionary<string, object>)row;
+                var obj = (Dictionary<string, object>) row;
+                Console.WriteLine($"Name: {obj["forename"]} {obj["surname"]} ({obj["gender"]})");
             }
         }
     }
