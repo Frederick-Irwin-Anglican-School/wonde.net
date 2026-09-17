6ff44b9c29fa6e4e8c9053016faf5c6ec538bec5
Jason Moore <jason.moore@fias.wa.edu.au>
2026-09-17 14:13:12 +0800

    ♻️ Refactor student iteration and using directives
    - 🎨 Refactored using directives to file-scoped style and grouped them at the top with a region
    - 🔥 Removed redundant nested class structure and unnecessary using statements inside the class
    - ♻️ Changed `iterate` to use `JsonElement` for parsing student properties, improving type-safety and modernizing the code
    - 💡 Updated main logic to call `iterate` after navigating pages for better clarity
    - 💡 Added TODO comment for future strongly-typed student objects

--- src/DotNetClient/TestWondeClient/Program.cs
+++ src/DotNetClient/TestWondeClient/Program.cs
@@ -1,42 +1,47 @@
 ﻿// TestWondeConsole
 
-namespace TestWondeClient
+namespace TestWondeClient;
+
+#region Using Directives
+using System;
+using System.Configuration;
+using System.Text.Json;
+using Wonde;
+#endregion
+
+internal class Program
 {
-    #region Using Directives
-    using System;
-    using System.Collections.Generic;
-    using System.Configuration;
-    using Wonde;
-    #endregion
-
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var client = new Client(ConfigurationManager.AppSettings["API_TOKEN"]);
+        var school = client.school(ConfigurationManager.AppSettings["SCHOOL_ID"]);
+
+        var students = school.students.all();
+
+        iterate(students);
+        if (students.nextPage())
         {
-            var client = new Client(ConfigurationManager.AppSettings["API_TOKEN"]);
-            var school = client.school(ConfigurationManager.AppSettings["SCHOOL_ID"]);
-
-            var students = school.students.all();
-
             iterate(students);
-            if (students.nextPage())
-            {
-                iterate(students);
-            }
-
-            if (students.previousPage())
-            {
-                iterate(students);
-            }
         }
 
-        static void iterate(ResultIterator students)
+        if (students.previousPage())
         {
-            foreach (var row in students)
-            {
-                var obj = (Dictionary<string, object>) row;
-                Console.WriteLine($"Name: {obj["forename"]} {obj["surname"]} ({obj["gender"]})");
-            }
+            iterate(students);
+        }
+    }
+
+    static void iterate(ResultIterator students)
+    {
+        foreach (var row in students)
+        {
+            // TODO: Convert to a strongly typed object instead of using JsonElement
+            var element = (JsonElement) row;
+
+            var forename = element.GetProperty("forename").GetString();
+            var surname  = element.GetProperty("surname").GetString();
+            var gender   = element.GetProperty("gender").GetString();
+
+            Console.WriteLine($"Name: {forename} {surname} ({gender})");
         }
     }
 }
